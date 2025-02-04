6bd928a8b3a55ce269125cf53874ef3eb0cfdf48
Lawn Meower <s_schmidt1993@outlook.de>
2025-02-04 15:07:45 +0100

    initial

---
+++ HexEditor.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Platform;
+using Microsoft.Maui.ApplicationModel;
+
+namespace HexEditor
+{
+    public partial class HexEditor : Microsoft.Maui.Controls.ContentView
+    {
+        public HexEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
